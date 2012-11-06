9d84c98069f84dcb993afb3765174d4a43e96f26
Mike Gershunovsky <mikebuckets@gmail.com>
2012-11-05 20:49:18 -0500

    added support for pipe characters

--- lib/keyword_parse.py
+++ lib/keyword_parse.py
@@ -14,7 +14,7 @@ def get_keyword_at_pos(line, col):
     # first look back until we find 2 spaces in a row, or reach the beginning
     i = col - 1
     while i >= 0:
-        if line[i] == "\t" or (line[i - 1] == ' ' and line[i] == ' '):
+        if line[i] == "\t" or ((line[i - 1] == ' ' or line[i - 1] == '|') and line[i] == ' '):
             break
         i -= 1
     begin = i + 1
@@ -22,7 +22,7 @@ def get_keyword_at_pos(line, col):
     # now look forward or until the end
     i = col # previous included line[col]
     while i < length:
-        if line[i] == "\t" or (line[i] == " " and len(line) > i and line[i + 1] == " "):
+        if line[i] == "\t" or (line[i] == " " and len(line) > i and (line[i + 1] == " " or line[i + 1] == '|')):
             break
         i += 1
     end = i
